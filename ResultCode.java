eb6c73c265ae1d2d1a7dc703be140d401aba4973
Mnsx_x <xx1527030652@gmail.com>
2022-09-22 23:51:07 +0800

    message

--- ResultCode.java
+++ ResultCode.java
@@ -1,10 +1,21 @@
-package top.mnsx.sks.result;
+package top.mnsx.second_kill_system.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @BelongsProject: second_kill_system
  * @User: Mnsx_x
- * @CreateTime: 2022/9/22 18:13
+ * @CreateTime: 2022/9/22 19:58
  * @Description: 响应码
  */
-public interface ResultCode {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResultCode {
+    private int code;
+    private String message;
+
+    public static ResultCode SUCCESS = new ResultCode(0, "success");
 }
